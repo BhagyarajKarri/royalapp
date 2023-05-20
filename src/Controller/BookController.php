48be11745973b58fd74bba9820881f2c3df2b629
BhagyarajKarri <59785856+BhagyarajKarri@users.noreply.github.com>
2023-05-20 20:04:53 +0530

    updated add book form
    Note: due to the post api for add book is not clear i have not written the backend code for it

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -11,15 +11,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class BookController extends AbstractController
 {
     /**
-     * @Route("/books/new", name="book_new")
+     * @Route("/books/add", name="books_add")
      */
-    public function addBook(Request $request): Response
+    public function add(Request $request): Response
     {
         // Handle the form submission to add a new book
         // For example, using Symfony's form handling
 
         // Perform any necessary actions after adding the book
+        $client = new Client([
+                'verify' => false, // Disable SSL verification
+            ]);
 
-        return $this->redirectToRoute('author_list');
+        $response = $client->get('https://candidate-testing.api.royal-apps.io/api/v2/authors', [
+                                'headers' => [
+                                    'Authorization' => $request->getSession()->get('access_token'),
+                                ],
+                        ]);
+        $authorss = json_decode($response->getBody()->getContents(), true);
+        $authors = $authorss['items'];
+
+        return $this->render('book/add.html.twig',[
+            'authors' => $authors,
+        ]);
     }
 }
